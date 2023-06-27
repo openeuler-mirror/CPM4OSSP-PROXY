42e3959234ab9e41c4a19d574a7a912239119080
zywang_dev <dev12101@linx-info.com>
2023-06-27 16:20:22 +0800

    增加登录校验服务

---
+++ proxy/src/main/java/io/mpms/controller/IndexController.java
@@ -0,0 +1,30 @@
+package io.mpms.controller;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.system.SystemUtil;
+import cn.jiangzeyin.common.JsonMessage;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.common.BaseAgentController;
+import io.jpom.common.JpomManifest;
+import io.jpom.common.RemoteVersion;
+import io.jpom.common.interceptor.NotAuthorize;
+import io.jpom.model.data.ProjectInfoModel;
+import io.jpom.service.WhitelistDirectoryService;
+import io.jpom.service.manage.ProjectInfoService;
+import io.jpom.util.JvmUtil;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 首页
+ *
+ */
+@RestController
+public class IndexController extends BaseAgentController {
+
+}
