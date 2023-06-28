ae402b2dd3d445579394dd6828ea56317b4f1e54
zywang_dev <dev12101@linx-info.com>
2023-06-28 09:41:14 +0800

    延时任务数据库映射

---
+++ proxy/src/main/java/io/mpms/dao/DelayedTaskDao.java
@@ -0,0 +1,20 @@
+package io.mpms.dao;
+
+import io.jpom.model.data.DelayedTask;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * (Delayedtask)表数据库访问层
+ *
+ *
+ * @since 2021-10-21 14:34:09
+ */
+@Mapper
+public interface DelayedTaskDao {
+
+
+
+}
