42135f76c9324243fba88c22e6d5c9df4df9ec32
zywang_dev <dev12101@linx-info.com>
2023-06-28 09:39:57 +0800

    审计数据库映射

---
+++ proxy/src/main/java/io/mpms/dao/MiniSysLogDao.java
@@ -0,0 +1,21 @@
+package io.mpms.dao;
+
+import io.jpom.model.data.MiniSysLog;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 凝思系统软件安装管理平台系统日志表(Minisyslog)表数据库访问层
+ *
+ *
+ * @since 2021-10-22 09:53:54
+ */
+@Mapper
+public interface MiniSysLogDao {
+
+
+
+
+}
