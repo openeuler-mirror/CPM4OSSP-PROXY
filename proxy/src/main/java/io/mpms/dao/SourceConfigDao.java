115db6b778027061b6864f0b1f53ec016ea22773
zywang_dev <dev12101@linx-info.com>
2023-06-28 09:38:24 +0800

    源配置数据库映射

---
+++ proxy/src/main/java/io/mpms/dao/SourceConfigDao.java
@@ -0,0 +1,19 @@
+package io.mpms.dao;
+
+import io.jpom.model.system.SourceConfig;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * (Sourceconfig)表数据库访问层
+ *
+ *
+ * @since 2021-10-25 17:05:11
+ */
+@Mapper
+public interface SourceConfigDao {
+
+
+}
