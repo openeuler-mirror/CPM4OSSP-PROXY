08520ce67836d14ca295eda337f7f4231d68314e
zywang_dev <dev12101@linx-info.com>
2023-06-29 17:34:38 +0800

    通过id查询审计数据

--- proxy/src/main/java/io/mpms/dao/MiniSysLogDao.java
+++ proxy/src/main/java/io/mpms/dao/MiniSysLogDao.java
@@ -15,7 +15,13 @@ import java.util.List;
 @Mapper
 public interface MiniSysLogDao {
 
-
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    MiniSysLog queryById(Integer id);
 
 
 }
