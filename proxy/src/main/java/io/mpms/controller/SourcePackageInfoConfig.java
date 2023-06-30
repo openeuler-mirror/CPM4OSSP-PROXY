8433847aef3ed57d8582e0564b6a9db4fb09c6f8
zywang_dev <dev12101@linx-info.com>
2023-06-30 17:22:28 +0800

    源配置索引路径value初始化

--- proxy/src/main/java/io/mpms/controller/SourcePackageInfoConfig.java
+++ proxy/src/main/java/io/mpms/controller/SourcePackageInfoConfig.java
@@ -10,4 +10,9 @@ public class SourcePackageInfoConfig {
     public static String getSourcePackageInfoIndexPath() {
         return sourcePackageInfoIndexPath;
     }
+
+    @Value("${pakgeinfo.indexPath:/usr/share/mpms_proxy/data_storage/index}")
+    public void setSourcePackageInfoIndexPath(String sourcePackageInfoIndexPath) {
+        SourcePackageInfoConfig.sourcePackageInfoIndexPath = sourcePackageInfoIndexPath;
+    }
 }
