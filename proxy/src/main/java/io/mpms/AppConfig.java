7cbff8eb9a4f3300e37f4dc22d3194cbd16b4fd8
zywang <dev12101@linx-info.com>
2024-12-30 14:19:40 +0800

    增加文件配置类

---
+++ proxy/src/main/java/io/mpms/AppConfig.java
@@ -0,0 +1,26 @@
+package io.jpom;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AppConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(7);
+        executor.setMaxPoolSize(42);
+        executor.setQueueCapacity(11);
+        executor.setThreadNamePrefix("ManageCommand-Thread");
+        executor.initialize();
+        return executor;
+    }
+
+
+}
